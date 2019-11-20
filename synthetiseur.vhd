80b1123471da9b7c01cae9e3f9961872f9c3006e
13440 <durandama@outlook.fr>
2019-11-20 13:18:28 +0100

    trame ADC

--- synthetiseur.vhd
+++ synthetiseur.vhd
@@ -10,7 +10,11 @@ library ieee ;
 		MAX10_CLK1_50 : in std_logic;
 		KEY : in  bit_vector(1 downto 0);
 		SW : in std_logic_vector(9 downto 0);
+<<<<<<< Updated upstream
 		GPIO : buffer std_logic_vector (10 downto 0)
+=======
+		GPIO : buffer std_logic_vector (35 downto 0)
+>>>>>>> Stashed changes
 		); 
     END synthetiseur;
 	 
@@ -26,6 +30,10 @@ library ieee ;
 	 Signal SDI : std_logic;
 	 Signal CSDAC : std_logic;
 	 Signal SCK : std_logic;
+	 Signal CSADC : std_logic;
+	 Signal SCKADC : std_logic;
+	 Signal adressADC : integer range 0 to 34;
+	 Signal SAADC : std_logic;
 	 
 	 Signal CSMCP : std_logic;
 	 Signal SI : std_logic;
@@ -54,8 +62,9 @@ library ieee ;
 			
 			begin
 			if rising_edge(horl100)
-			then adressfal <= adressfal + 1;
+				then adressfal <= adressfal + 1;
 				case adressfal is
+<<<<<<< Updated upstream
 				when 0 => CSDAC <= '0'; SDI <= '1';   --choix de la voie B
 				When 1 => SCK <= '1';
 				When 2 => SDI <= '0'; SCK<='0';
@@ -101,10 +110,58 @@ library ieee ;
 				when 42 => SCK <= '0'; SDI <= '0'; CSDAC <= '1';
 				when 43 => SCK <= '0'; SDI <= '0'; CSDAC <= '1'; adressfal <= 0;
 				when others => SCK <= '0';
+=======
+					when 0 => CS <= '0'; SDI <= '1';   --choix de la voie B
+					When 1 => SCK <= '1';
+					When 2 => SDI <= '0'; SCK<='0';
+					when 3 => SCK <= '1'; 
+					when 4 => SCK <= '0'; SDI <='1';
+					when 5 => SCK <= '1'; 
+					when 6 => SCK <= '0';
+					when 7 => SCK <= '1';
+					when 8 => SCK <= '0'; SDI <= SW(0);   
+					When 9 => SCK <= '1'; 
+					When 10 => SCK <= '0'; SDI <= SW(1);
+					when 11 => SCK <= '1'; 
+					when 12 => SCK <= '0'; SDI <= SW(2);
+					when 13 => SCK <= '1'; 
+					when 14 => SCK <= '0'; SDI <= SW(3);
+					when 15 => SCK <= '1'; 
+					When 16 => SCK <= '0'; SDI <= SW(4);
+					When 17 => SCK <= '1'; 
+					when 18 => SCK <= '0'; SDI <= SW(5);
+					when 19 => SCK <= '1'; 
+					when 20 => SCK <= '0'; SDI <= SW(6);
+					when 21 => SCK <= '1'; 
+					when 22 => SCK <= '0'; SDI <= SW(7);
+					when 23 => SCK <= '1'; 
+					when 24 => SCK <= '0'; SDI <= SW(8);
+					when 25 => SCK <= '1'; 
+					when 26 => SCK <= '0'; SDI <= SW(9);
+					when 27 => SCK <= '1'; 
+					when 28 => SCK <= '0'; SDI <= '0';
+					when 29 => SCK <= '1'; 
+					when 30 => SCK <= '0'; SDI <= '0';
+					when 31 => SCK <= '1'; CS <= '0';
+					when 32 => SCK <= '0';
+					when 33 => SCK <= '0'; SDI <= '0'; CS <= '1';
+					when 34 => SCK <= '0'; SDI <= '0'; CS <= '1';
+					when 35 => SCK <= '0'; SDI <= '0'; CS <= '1';
+					when 36 => SCK <= '0'; SDI <= '0'; CS <= '1';
+					when 37 => SCK <= '0'; SDI <= '0'; CS <= '1';
+					when 38 => SCK <= '0'; SDI <= '0'; CS <= '1';
+					when 39 => SCK <= '0'; SDI <= '0'; CS <= '1';
+					when 40 => SCK <= '0'; SDI <= '0'; CS <= '1';
+					when 41 => SCK <= '0'; SDI <= '0'; CS <= '1';
+					when 42 => SCK <= '0'; SDI <= '0'; CS <= '1';
+					when 43 => SCK <= '0'; SDI <= '0'; CS <= '1'; adressfal <= 0;
+					when others => SCK <= '0';
+>>>>>>> Stashed changes
 				end case;
-				end if;
+			end if;
 			GPIO(0) <= SCK;
 			GPIO(1) <= SDI;
+<<<<<<< Updated upstream
 			GPIO(2) <= CSDAC;
 			end process DACTRAM;
 			
@@ -251,5 +308,60 @@ library ieee ;
 			end process MCPTRAM;
 			
 	 
+=======
+			GPIO(2) <= CS;
+			end process test;
+			
+			
+			ADC : Process (horl100)
+			
+			begin
+			if rising_edge (horl100)
+				then adressADC <= adressADC + 1;
+				case adressADC is
+					when 0 => CSADC <= '1';
+					when 1 => SCKADC <= '1'; SAADC <= '0';
+					when 2 => SCKADC <= '0';
+					when 3 => SCKADC <= '1';
+					when 4 => SCKADC <= '0';
+					when 5 => SCKADC <= '1';
+					when 6 => SCKADC <= '0';
+					when 7 => SCKADC <= '1';
+					when 8 => SCKADC <= '0';
+					when 9 => SCKADC <= '1';
+					when 10 => SCKADC <= '0';
+					when 11 => SCKADC <= '1';
+					when 12 => SCKADC <= '0';
+					when 13 => SCKADC <= '1';
+					when 14 => SCKADC <= '0';
+					when 15 => SCKADC <= '1';
+					when 16 => SCKADC <= '0';
+					when 17 => SCKADC <= '0';
+					when 18 => SCKADC <= '1';
+					when 19 => SCKADC <= '0';
+					when 20 => SCKADC <= '1'; CSADC <= '0';
+					when 21 => SCKADC <= '0';
+					when 22 => SCKADC <= '1';
+					when 23 => SCKADC <= '0';
+					when 24 => SCKADC <= '0';
+					when 25 => SCKADC <= '1'; 
+					when 26 => SCKADC <= '0';
+					when 27 => SCKADC <= '1'; 
+					when 28 => SCKADC <= '0';
+					when 29 => SCKADC <= '1'; 
+					when 30 => SCKADC <= '0';
+					when 31 => SCKADC <= '1';
+					when 32 => SCKADC <= '0';
+					when 33 => SCKADC <= '1';
+					when 34 => SCKADC <= '0'; adressADC <= 0; CSADC <= '1';
+					when others => SCKADC <= '0';
+				end case;
+			end if;
+			GPIO(33) <= SCKADC;
+			GPIO(34) <= SAADC;
+			GPIO(35) <= CSADC;
+					
+			end process ADC;
+>>>>>>> Stashed changes
 	 
 	 END architecture;
