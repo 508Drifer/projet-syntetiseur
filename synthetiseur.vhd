3c53b628157da4ed3ddb7f62dbe50d28efe8340f
Loïc <loic.savornin@yahoo.fr>
2019-11-20 13:27:51 +0100

    base

--- synthetiseur.vhd
+++ synthetiseur.vhd
@@ -105,53 +105,6 @@ library ieee ;
 				when 42 => SCK <= '0'; SDI <= '0'; CSDAC <= '1';
 				when 43 => SCK <= '0'; SDI <= '0'; CSDAC <= '1'; adressfal <= 0;
 				when others => SCK <= '0';
-
-					when 0 => CS <= '0'; SDI <= '1';   --choix de la voie B
-					When 1 => SCK <= '1';
-					When 2 => SDI <= '0'; SCK<='0';
-					when 3 => SCK <= '1'; 
-					when 4 => SCK <= '0'; SDI <='1';
-					when 5 => SCK <= '1'; 
-					when 6 => SCK <= '0';
-					when 7 => SCK <= '1';
-					when 8 => SCK <= '0'; SDI <= SW(0);   
-					When 9 => SCK <= '1'; 
-					When 10 => SCK <= '0'; SDI <= SW(1);
-					when 11 => SCK <= '1'; 
-					when 12 => SCK <= '0'; SDI <= SW(2);
-					when 13 => SCK <= '1'; 
-					when 14 => SCK <= '0'; SDI <= SW(3);
-					when 15 => SCK <= '1'; 
-					When 16 => SCK <= '0'; SDI <= SW(4);
-					When 17 => SCK <= '1'; 
-					when 18 => SCK <= '0'; SDI <= SW(5);
-					when 19 => SCK <= '1'; 
-					when 20 => SCK <= '0'; SDI <= SW(6);
-					when 21 => SCK <= '1'; 
-					when 22 => SCK <= '0'; SDI <= SW(7);
-					when 23 => SCK <= '1'; 
-					when 24 => SCK <= '0'; SDI <= SW(8);
-					when 25 => SCK <= '1'; 
-					when 26 => SCK <= '0'; SDI <= SW(9);
-					when 27 => SCK <= '1'; 
-					when 28 => SCK <= '0'; SDI <= '0';
-					when 29 => SCK <= '1'; 
-					when 30 => SCK <= '0'; SDI <= '0';
-					when 31 => SCK <= '1'; CS <= '0';
-					when 32 => SCK <= '0';
-					when 33 => SCK <= '0'; SDI <= '0'; CS <= '1';
-					when 34 => SCK <= '0'; SDI <= '0'; CS <= '1';
-					when 35 => SCK <= '0'; SDI <= '0'; CS <= '1';
-					when 36 => SCK <= '0'; SDI <= '0'; CS <= '1';
-					when 37 => SCK <= '0'; SDI <= '0'; CS <= '1';
-					when 38 => SCK <= '0'; SDI <= '0'; CS <= '1';
-					when 39 => SCK <= '0'; SDI <= '0'; CS <= '1';
-					when 40 => SCK <= '0'; SDI <= '0'; CS <= '1';
-					when 41 => SCK <= '0'; SDI <= '0'; CS <= '1';
-					when 42 => SCK <= '0'; SDI <= '0'; CS <= '1';
-					when 43 => SCK <= '0'; SDI <= '0'; CS <= '1'; adressfal <= 0;
-					when others => SCK <= '0';
-
 				end case;
 			end if;
 			GPIO(0) <= SCK;
@@ -300,7 +253,17 @@ library ieee ;
 			GPIO(4) <= SI;
 			GPIO(5) <= CSMCP;
 			GPIO(6) <= horl100;
-			end process MCPTRAM;
+			end process MCPTRAM;	
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 			
 			
 			ADC : Process (horl100)		
